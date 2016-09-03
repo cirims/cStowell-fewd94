c0cd854924df1d0c9ff12cf91613bfc0e92f5cc7
Ciri <YOUR EMAIL ADDRESS>
2016-09-02 21:43:01 -0400

    homework

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,30 @@
+
+document.write('this is some text');
+document.write('<br>');
+document.write(10*10);
+document.write('<br>');
+document.write(100/10);
+document.write('<br>');
+document.write(50-17);
+document.write('<br>');
+document.write(50+17);
+document.write('<br>');
+document.write('Ciri'+' Stowell');
+document.write('<br>');
+
+var num1 = 10*10;
+document.write(num1);
+document.write('<br>');
+
+console.log(num1);
+
+var num2=55;
+console.log("num2:"+num2);
+
+// function
+function doSomething(){
+	console.log('this is working');
+}
+
+// call the function
+doSomething();
